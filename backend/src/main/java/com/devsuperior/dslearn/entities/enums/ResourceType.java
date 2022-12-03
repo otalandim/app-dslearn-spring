f0e69fb4b268d3f1bf5e95e63cc351007f8a4eae
Otávio Landim <otavio.landim@gmail.com>
2022-12-03 11:42:02 -0300

    Adding resource entity/enum

---
+++ backend/src/main/java/com/devsuperior/dslearn/entities/enums/ResourceType.java
@@ -0,0 +1,5 @@
+package com.devsuperior.dslearn.entities.enums;
+
+public enum ResourceType {
+    LESSON_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-12-03T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-03T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-12-03T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-03T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tira suas dúvidas', 2, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', 0, 1);
