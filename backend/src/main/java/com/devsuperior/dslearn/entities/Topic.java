34f24df043e9d00cbe0f40b308bd20cac082054d
Otávio Landim <otavio.landim@gmail.com>
2022-12-03 22:29:11 -0300

    Adding reply entity and seed

--- backend/src/main/java/com/devsuperior/dslearn/entities/Topic.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Topic.java
@@ -3,7 +3,9 @@ package com.devsuperior.dslearn.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -34,108 +36,127 @@ public class Topic implements Serializable {
     @JoinColumn(name = "lesson_id")
     private Lesson lesson;
 
+    @ManyToOne
+    @JoinColumn(name = "reply_id")
+    private Reply answer;
+
     @ManyToMany
     @JoinTable(name = "tb_topic_likes",
             joinColumns = @JoinColumn(name = "topic_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id"))
     private Set<User> likes = new HashSet<>();
 
+    @OneToMany(mappedBy = "topic")
+    private List<Reply> replies = new ArrayList<>();
+
 	public Topic() {
         }
 
 	public Topic(Long id, String title, String body, Instant moment, User author, Offer offer, Lesson lesson) {
-            super();
-            this.id = id;
-            this.title = title;
-            this.body = body;
-            this.moment = moment;
-            this.author = author;
-            this.offer = offer;
-            this.lesson = lesson;
-        }
+        super();
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.moment = moment;
+        this.author = author;
+        this.offer = offer;
+        this.lesson = lesson;
+    }
 
-        public Long getId() {
-            return id;
-        }
+    public Long getId() {
+        return id;
+    }
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public String getTitle() {
-            return title;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-        public String getBody() {
-            return body;
-        }
+    public String getBody() {
+        return body;
+    }
 
-        public void setBody(String body) {
-            this.body = body;
-        }
+    public void setBody(String body) {
+        this.body = body;
+    }
 
-        public Instant getMoment() {
-            return moment;
-        }
+    public Instant getMoment() {
+        return moment;
+    }
 
-        public void setMoment(Instant moment) {
-            this.moment = moment;
-        }
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
 
-        public User getAuthor() {
-            return author;
-        }
+    public User getAuthor() {
+        return author;
+    }
 
-        public void setAuthor(User author) {
-            this.author = author;
-        }
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 
-        public Offer getOffer() {
-            return offer;
-        }
+    public Offer getOffer() {
+        return offer;
+    }
 
-        public void setOffer(Offer offer) {
-            this.offer = offer;
-        }
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
 
-        public Lesson getLesson() {
-            return lesson;
-        }
+    public Lesson getLesson() {
+        return lesson;
+    }
 
-        public void setLesson(Lesson lesson) {
-            this.lesson = lesson;
-        }
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
 
-        public Set<User> getLikes() {
-            return likes;
-        }
+    public Set<User> getLikes() {
+        return likes;
+    }
 
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((id == null) ? 0 : id.hashCode());
-            return result;
-        }
+    public Reply getAnswer() {
+        return answer;
+    }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Topic other = (Topic) obj;
-            if (id == null) {
-                if (other.id != null)
-                    return false;
-            } else if (!id.equals(other.id))
-                return false;
+    public void setAnswer(Reply answer) {
+        this.answer = answer;
+    }
+
+    public List<Reply> getReplies() {
+        return replies;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
             return true;
-        }
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Topic other = (Topic) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
