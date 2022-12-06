ae29d913787579a7e2b0cb5b378bd076761f7c8e
Otávio Landim <otavio.landim@gmail.com>
2022-12-05 21:50:22 -0300

    Adding pre-authorized method

---
+++ backend/src/main/java/com/devsuperior/dslearn/resources/DeliverResource.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslearn.resources;
+
+import com.devsuperior.dslearn.dto.DeliverRevisionDTO;
+import com.devsuperior.dslearn.services.DeliverService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverResource {
+
+    @Autowired
+    private DeliverService service;
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody DeliverRevisionDTO dto){
+        service.saveRevision(id, dto);
+        return ResponseEntity.noContent().build();
+    }
+}
